144b4b7130afbad4e4cb2776c142e58a05754e23
NucciTheBoss <jason.nucciarone@canonical.com>
2023-04-06 13:59:33 -0400

    refactor: Remove compound status

--- src/charm.py
+++ src/charm.py
@@ -11,7 +11,6 @@ from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
 from ops.model import ActiveStatus, BlockedStatus
-from utils.status import Status, StatusPool
 
 logger = logging.getLogger(__name__)
 
@@ -23,7 +22,6 @@ class NFSServerProxyCharm(CharmBase):
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self._status_pool = StatusPool(self)
         self._stored.set_default(endpoint=None)
         self._nfs_share = NFSProvides(self, "nfs-share")
         self.framework.observe(self.on.config_changed, self._on_config_changed)
@@ -32,16 +30,15 @@ class NFSServerProxyCharm(CharmBase):
     def _on_config_changed(self, _) -> None:
         """Handle updates to NFS server proxy configuration."""
         if (endpoint := self.config.get("endpoint")) is None:
-            self._status_pool.add(
-                Status(label="no_endpoint", status=BlockedStatus("No configured endpoint"))
-            )
+            self.unit.status = BlockedStatus("No configured endpoint")
+            return
         elif endpoint is not None and self._stored.endpoint is None:
             logger.debug(f"Setting NFS share endpoint to {endpoint}")
             self._stored.endpoint = endpoint
         else:
             logger.warning(f"Endpoint can only be set once. Ignoring {endpoint}")
 
-        self._status_pool.add(Status(label="ready", status=ActiveStatus("Exporting share")))
+        self.unit.status = ActiveStatus("Exporting share")
 
     def _on_share_requested(self, event: ShareRequestedEvent) -> None:
         """Handle when NFS client requests an NFS share."""
@@ -53,7 +50,7 @@ class NFSServerProxyCharm(CharmBase):
         )
         if self._stored.endpoint is None:
             logger.warning("Deferring ShareRequested event because endpoint is not set")
-            self._status_pool.add(Status(label="no_endpoint"))
+            self.unit.status = BlockedStatus("No configured endpoint")
             event.defer()
         else:
             logger.debug(f"Setting {self._stored.endpoint} as the NFS share endpoint")
--- src/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2023 Canonical Ltd.
-# See LICENSE file for licensing details.
-
-"""Internal utilities used throughout the NFS server proxy operator."""
